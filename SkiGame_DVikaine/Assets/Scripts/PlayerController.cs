32acd84061ab08febd1cb7ebbe435061b86ab7bf
Randerara <daniela.vikaine@gmail.com>
2026-04-13 15:23:52 +0300

    PlayerController - adding force

--- SkiGame_DVikaine/Assets/Scripts/PlayerController.cs
+++ SkiGame_DVikaine/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private InputAction move;
-
+    [SerializeField] private float rotSpeed = 45, speed = 20;
     void Awake()
     {
         move = InputSystem.actions.FindAction("Player/Move");
+        rb = GetComponent<Rigidbody>();
     }
     
-    void Update()
+    void FixedUpdate()
     {
+       rb.AddForce(transform.forward * speed * Time.fixedDeltaTime);
        Vector2 moveInput = move.ReadValue<Vector2>();
-       Debug.Log("x: " + moveInput.x + "y:" + moveInput.y);
+       transform.Rotate(0, -moveInput.x * rotSpeed * Time.fixedDeltaTime, 0);
     }
 }
