177eb52749cf53805564678bf708717d48ea8336
danivik9 <daniela.vikaine@gmail.com>
2026-04-27 15:48:27 +0300

    Race time

--- SkiGame_DVikaine/Assets/Scripts/GameManager.cs
+++ SkiGame_DVikaine/Assets/Scripts/GameManager.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
-
+using System;
 public class GameManager : MonoBehaviour
 {
+    private DateTime raceStart;
+    private TimeSpan raceTime;
+    private bool racing=false;
     public delegate void TimerEvent();
     private void OnEnable()
     {
         StartGate.StartRace += OnRaceStart;
+        FinishGate.FinishRace += OnRaceFinish;
     }
     void OnRaceStart()
     {
+        racing = true;
+        raceStart = DateTime.Now;
         Debug.Log("Race Started");
+    }
+    void OnRaceFinish()
+    {
+        racing = false;
+        Debug.Log("Race Finished");
+    }
+
+    private void Update()
+    {
+        if(racing)
+            raceTime = DateTime.Now - raceStart;
+        Debug.Log("Race Time: " + raceTime);
     }
 }
